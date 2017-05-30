66be1488a4ba88643d808fecdf9cf2550f61933b
wind <bupt_ljy@163.com>
2017-05-31 00:12:14 +0800

    fixed

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
-from fabric.api import local
+from fabric.api import *
 
-
+env.hosts = ['root@101.200.171.13']
 
 def test():
     local('./manage.py test')
 
 def add():
-    local('git add -A')
+    local('git checkout develop && git add -A')
 
 def commit():
     local('git commit -am "fixed" ')
@@ -17,8 +17,17 @@ def fetch():
 def push():
     local('git push -u origin develop')
 
+
 def deploy():
+    blog_dir = '/server/projects/wind_blog/blog-project'
     test()
     add()
     commit()
     push()
+    # remote deploy
+    with cd(blog_dir):
+        run('source ../bin/activate')
+        run('git checkout develop && git pull origin develop')
+        run('pkill gunicorn')
+        run('gunicorn wind_blog.wsgi -c gunicorn_setting.py')
+
